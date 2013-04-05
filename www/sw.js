6fa5aa9c6df3288e422a6ec766d45781f99c2130
brunosan <brunosan@gmail.com>
2013-04-05 02:27:26 -0400

    search function

--- www/sw.js
+++ www/sw.js
@@ -1,4 +1,78 @@
- // Create map
+//Read data
+
+function readAllData() {
+	//read documentation JSON
+	d3.json("events.geojson", function(json) {
+		// Put the object into storage
+		localStorage.setItem('locations', JSON.stringify(json));
+		// Retrieve the object from storage
+		var retrievedObject = localStorage.getItem('locations');
+		console.log('locations saved in local storage: ', JSON.parse(retrievedObject));
+	});
+	addMeasureInput()
+}
+
+//search function
+function addMeasureInput() {
+	YUI().use("autocomplete", "autocomplete-filters", "autocomplete-highlighters", function(Y) {
+		//skin
+		Y.one('body').addClass('yui3-skin-sam');
+		//Array source
+		var locs = getLocationsList();
+		Y.one('#ac-input').plug(Y.Plugin.AutoComplete, {
+			resultFilters: 'phraseMatch',
+			resultHighlighter: 'phraseMatch',
+			source: locs,
+			on: {
+				select: function() {
+					console.log("Locations Selected!");
+				}
+			},
+			after: {
+				select: function(o) {
+					showLoc(o.result.raw);
+				}
+			}
+		});
+	});
+};
+function checkEnter(e) {
+	var key = e.keyCode || e.which;
+	if (key == 13) {
+		console.log('Enter!');
+	}
+}
+
+function getLocationsList() {
+	var locs = JSON.parse(localStorage.getItem('locations'));
+	
+	var locs_list = [];
+	for (l in locs["features"]) {
+		var li = locs["features"][l]["properties"]["title"];
+		if (locs_list.indexOf(li) == -1){
+			locs_list.push(li);
+		}
+	}
+	return locs_list;
+}
+
+function showLoc(loc){
+	var locs = JSON.parse(localStorage.getItem('locations'));
+	var la,lo;
+	var locs_list = [];
+	for (l in locs["features"]) {
+		var li = locs["features"][l]["properties"]["title"];
+		if (loc==li){
+			la=locs["features"][l]["geometry"]["coordinates"][0];
+			lo=locs["features"][l]["geometry"]["coordinates"][1];
+			var map = mapbox.map('map');
+			map.ease.location({ lat: la, lon: lo }).zoom(10).optimal();
+			return; //to avoid when more than one place is present
+		}
+	}
+}
+
+// Create map
   mapbox.auto('map', 'mapbox.world-light', function(map, tiledata) {
 
     //Add GeoJSON markers
@@ -47,7 +121,12 @@
 		});
 		//Cycle through markers? http://mapbox.com/mapbox.js/example/cycle-markers/
 
- 	// Attribute map
+ 	//emtpy on parent 
+	MM.addEvent(map.parent, 'click', function() {
+      alert.innerHTML = '';
+  	});
+
+	// Attribute map
   	map.ui.attribution.add()
       .content('<a href="http://mapbox.com/about/maps">Terms &amp; Feedback</a>');
   
@@ -56,7 +135,7 @@
 	map.centerzoom({
         lat: 27.6,
         lon: -20.07
-    }, 2);
+    }, 2,true);
 
 	var xlat,xlon,xz;
 	if (navigator.geolocation)
